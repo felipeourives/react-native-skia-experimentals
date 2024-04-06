18ee2509fe40ef86ecb9b315aa5317617b6fc656
Felipe Ourives <felipe.ourives@gmail.com>
2024-04-06 02:05:09 +0200

    move to center after tap

--- constellations/src/menu/screens.tsx
+++ constellations/src/menu/screens.tsx
@@ -1,10 +1,12 @@
 import { ConstellationGestureMove } from '../constellation-gesture-move/constellation-gesture-move';
 import { ConstellationGesture } from '../constellation-gesture/constellation-gesture';
+import { ConstellationMoveCenter } from '../constellation-move-center/constellation-move-center';
 import { Constellation } from '../constellation/constellation';
 
 export const Screens = [
   { Name: "Highlight Stars", Component: ConstellationGesture },
   { Name: "Move Stars", Component: ConstellationGestureMove },
+  { Name: "Move Starts to center", Component: ConstellationMoveCenter },
   { Name: "Constellations", Component: Constellation }
 ];
 
